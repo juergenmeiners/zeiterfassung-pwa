fcd2451e679778f19584219b0c6d5f4b13ed8eff
juergenmeiners <juergen.meiners@gmail.com>
2025-09-15 18:50:41 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -11,3 +11,16 @@ self.addEventListener("fetch", (event) => {
     fetch(event.request).catch(() => caches.match(event.request))
   );
 });
+// Push Event abfangen
+self.addEventListener("push", (event) => {
+  const data = event.data ? event.data.json() : {};
+  const title = data.title || "Erinnerung";
+  const options = {
+    body: data.body || "Es ist Zeit!",
+    icon: "/icon-192.png",
+    badge: "/icon-192.png",
+    vibrate: [500, 200, 500],
+    requireInteraction: true // bleibt sichtbar, bis Nutzer reagiert
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
+});
