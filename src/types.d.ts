d971a6cc9bb0a23b959eb98f2628dfef9dfc0963
juergenmeiners <juergen.meiners@gmail.com>
2025-09-16 16:08:11 +0200

    Create types.d.ts

---
+++ src/types.d.ts
@@ -0,0 +1,22 @@
+interface WakeLockSentinel {
+  release(): Promise<void>;
+}
+
+interface Navigator {
+  wakeLock?: {
+    request(type: 'screen'): Promise<WakeLockSentinel>;
+  };
+  webkitAudioContext?: typeof AudioContext;
+}
+
+interface Window {
+  webkitAudioContext?: typeof AudioContext;
+}
+
+interface NotificationOptions {
+  vibrate?: number[];
+}
+
+declare namespace NodeJS {
+  interface Timeout {}
+}
